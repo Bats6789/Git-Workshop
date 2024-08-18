3ff9fb3a00e192d746214fe4bddf8e2befa4f3bf
Blake Wingard <bats23456789@gmail.com>
2024-08-18 22:20:22 +0530

    Created step 7 for Linux.

---
+++ Linux/step7.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd demo
+
+git checkout -b feature3
+cp ../../resources/file7_1.txt file7.txt
+git add file7.txt
+git commit -m "Created file7.txt"
+
+git switch master
+cp ../../resources/file7_2.txt file7.txt
+git add file7.txt
+git commit -m "Created file7.txt"
